800b6ccb796a33ff17fa2fa17cd7fa2b00e223af
nikhil <nsingh@cdac.in>
2025-09-21 00:28:23 +0530

    auth header created

--- src/components/AuthHeader/Index.tsx
+++ src/components/AuthHeader/Index.tsx
@@ -7,7 +7,7 @@ const AuthHeader = ({title}:{title:string}) => {
 
   return (
      <View style={style.container}>
-        <Pressable hitSlop={20} onPress={()=>console.log("auth Header")}>
+        <Pressable hitSlop={20}>
             <Image style={style.image} source=                {require('../../assets/auth_back.png')}></Image>
         </Pressable>
          <Text style={style.title}>{title}</Text>
